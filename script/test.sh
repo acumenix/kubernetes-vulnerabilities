9b10b1f0eeec1967fdcbc5dc74cd01104499adf6
Effi <effoeffi@users.noreply.github.com>
2020-03-03 14:09:13 +0200

    Running audit tests (#9)
    * Running audit tests script
    
    * circle
    
    * test script: pass K8S_VULNERABILITIES_BRANCH_NAME parameter to 'audit-itest' as the circleci branch, and wait up to 10 minutes for the job to complete

---
+++ script/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+[ -z "${CIRCLE_TOKEN_ORCA}" ] && echo "CIRCLE_TOKEN_ORCA can not be empty" exit 1
+
+echo "Running audit tests..."
+jobNumber=$(curl -s -X POST \
+    -H "Content-Type:application/json" \
+    -d \
+    '{ "build_parameters": {
+        "CIRCLE_JOB": "audit-itest",
+        "K8S_VULNERABILITIES_BRANCH_NAME": "'"${CIRCLE_BRANCH}"'"
+    }}' \
+    "https://circleci.com/api/v1.1/project/github/Tufin/orca?circle-token=${CIRCLE_TOKEN_ORCA}" | jq -r '.build_num')
+
+echo "Watting for audit test's job number: '${jobNumber}'"
+status="na"
+for i in $(seq 1 600); do
+    job=$(curl -s "https://circleci.com/api/v1.1/project/github/Tufin/orca/${jobNumber}?circle-token=${CIRCLE_TOKEN_ORCA}")
+    lifecycle=$(echo "$job" | jq -r '.lifecycle')
+    if [[ "${lifecycle}" == "finished" ]]; then
+      status=$(echo "$job" | jq -r '.status')
+      break
+    else
+      printf .
+    fi
+    sleep 1
+done
+
+echo ""
+echo "Tests completed with status: '$status'"
